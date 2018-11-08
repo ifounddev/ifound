7a3decd35f935ba90bc1b5b677361170d00b95c0
LuanLCosta <luancpereira@gmail.com>
2018-11-08 12:39:36 -0200

    teste Luan

--- server.js
+++ server.js
@@ -17,12 +17,13 @@ io.on("connection", function (socket){
     socket.on("CHAT", function(data){
     	const mensage = new chat({ usuario : data.usuario, message: data.message});
     	message.save().then (()=> console.log('gravou ' + data.message)) 
-        io.emit("CHAT", { usuario : data.usuario, message: data.message});
+  		io.to(`${socketId}`).emit('CHAT', { usuario : data.usuario, message: data.message});
+        // io.emit("CHAT", { usuario : data.usuario, message: data.message});
     });
 });
 
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var ip = process.env.OPENSHIFT_NODEJS_IP;
+var ip = process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0";
 
 http.listen(port, ip, function(){
     console.log("chat com sucesso!!!!!!!!!");
